31f2133e1aa16d4ed75cb61fa22b4e90074493e4
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-04-13 17:02:27 +0200

    Ignored files

---
+++ S-Innovations.Azure.TableStorageRepository.Test/UnitTest2.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.IO;
+using SInnovations.Azure.TableStorageRepository.TableRepositories;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Collections.Generic;
+
+namespace SInnovations.Azure.TableStorageRepository.Test
+{
+
+#if DEBUG
+    public class MyTestPoco
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Guid Id { get; set; }
+
+        public virtual ICollection<SubItem> Items { get; set; }
+    }
+
+    public class MyTestPocoEntity : TableEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Guid Id { get; set; }
+
+        public virtual ICollection<SubItem> Items { get; set; }
+    }
+
+    public class SubItem
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+    public class NewTableStorageContext : TableStorageContext
+    {
+        public NewTableStorageContext()
+            : base(new CloudStorageAccount(
+            new StorageCredentials("c1azuretests",File.ReadAllText("C:\\dev\\storagekey.txt")), true))
+        {
+
+            this.InsertionMode = InsertionMode.AddOrMerge;
+        }
+        protected override void OnModelCreating(TableStorageModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<SubItem>().HasKeys(t=>t.Name,t=>t.LastName).ToTable("testtable");
+            modelbuilder.Entity<MyTestPoco>()
+                .HasKeys(y => y.Name, y => y.LastName)
+                .WithCollectionOf(t => t.Items, (source, item) => (from i in source where i.Name == item.Name select i))
+                .ToTable("testsfds");
+            modelbuilder.Entity<MyTestPocoEntity>()
+                .HasKeys(y => y.Name, y => y.LastName)
+                .WithCollectionOf(t=>t.Items,(source,item) => (from i in source where i.Name == item.Name select i))
+                .ToTable("testsfds");
+           
+
+            
+            base.OnModelCreating(modelbuilder);
+        }
+
+        public ITableRepository<MyTestPoco> People { get; set; }
+        public ITableRepository<MyTestPocoEntity> People1 { get; set; }
+        public ITableRepository<SubItem> SubItems { get; set; }
+        
+    }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public async Task SelectAll()
+        {
+
+            var context = new NewTableStorageContext();
+
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad" });
+
+            await context.SaveChangesAsync();
+
+            var query = from ent in context.People select ent;
+
+            var result = query.ToArray();
+        }
+
+        [TestMethod]
+        public async Task SelectWithWhere()
+        {
+
+            var context = new NewTableStorageContext();
+
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad1" });
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad2" });
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad3" });
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad4" });
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad5" });
+            context.People.Add(new MyTestPoco { LastName = "as", Name = "adsad6" });
+
+            await context.SaveChangesAsync();
+
+            //This call should project name to PartitionKey
+            var query = from ent in context.People
+                        where ent.Name == "adsad1"
+                        select ent;
+
+            //The following query get build such only the Name column is
+            //retrieved from the server, and its mapped such it know to do
+            //the filter on partition key and not the name. (Model Builder)
+            var result = query.Select(t=>t.Name).ToArray();
+        }
+
+        [TestMethod]
+        public async Task SelectServerSideProjection()
+        {
+
+            var context = new NewTableStorageContext();
+
+            context.SubItems.Add(new SubItem { Name = "adsad1", LastName= "blasad1" });
+            context.SubItems.Add(new SubItem { Name = "adsad1", LastName = "blasad2" });
+            context.SubItems.Add(new SubItem { Name = "adsad2", LastName = "blasa3d" });
+            context.SubItems.Add(new SubItem { Name = "adsad2", LastName = "blasad4" });
+            
+            await context.SaveChangesAsync();
+
+
+            //This call should project name to PartitionKey
+            var query = from ent in context.People
+                        select new { ent.Name, ent.Age };
+
+            //The following query get build such only the Name column is
+            //retrieved from the server, and its mapped such it know to do
+            //the filter on partition key and not the name. (Model Builder)
+            var result = query.Select(t => t).Where(t=>true).Select(t=>t.Name).ToArray();
+
+            var withTableEntity = (from ent in context.People1
+                                  select new { ent.Name }).ToArray();
+
+            var adsad1 = await context.People1.FindByKeysAsync("adsad1", "as");
+            var subitems = (from ent in adsad1.Items
+                           select ent).ToArray();
+            var tst = (from ent in ((ITableRepository<SubItem>)adsad1.Items).parentQuery
+                      select ent).ToArray();
+
+            var allsubs = (from ent in context.SubItems select ent).ToArray();
+            
+        }
+    }
+#endif
+}
---
+++ S-Innovations.Azure.TableStorageRepository/Queryable/TableQueryProvider.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using SInnovations.Azure.TableStorageRepository.Queryable.Base;
+using SInnovations.Azure.TableStorageRepository.Queryable.Expressions;
+using SInnovations.Azure.TableStorageRepository.Queryable.Wrappers;
+using SInnovations.Azure.TableStorageRepository.TableRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SInnovations.Azure.TableStorageRepository.Queryable
+{
+    /// <summary>
+    ///     LINQ to Windows Azure Storage Table query provider.
+    ///     http://msdn.microsoft.com/en-us/library/windowsazure/dd894031.aspx
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type.</typeparam>
+    internal class TableQueryProvider<TEntity> : QueryProviderBase, IAsyncQueryProvider where TEntity : new()
+    {
+        private readonly TablePocoRepository<TEntity> _repository;
+        private readonly EntityTypeConfiguration<TEntity> _entityConfiguration;
+        private readonly QueryTranslator _queryTranslator;
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="cloudTable">Cloud table.</param>
+        /// <param name="entityConverter"></param>
+        internal TableQueryProvider(TablePocoRepository<TEntity> cloudTable, EntityTypeConfiguration<TEntity> entityConverter)
+        {
+            if (cloudTable == null)
+            {
+                throw new ArgumentNullException("cloudTable");
+            }
+
+            if (entityConverter == null)
+            {
+                throw new ArgumentNullException("entityConverter");
+            }
+
+            _repository = cloudTable;
+            _entityConfiguration = entityConverter;
+            _queryTranslator = new QueryTranslator(entityConverter.NamePairs);
+        }
+
+        /// <summary>
+        ///     Executes expression query.
+        /// </summary>
+        /// <param name="expression">Expression.</param>
+        /// <returns>Result.</returns>
+        public override object Execute(Expression expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            var result = new TranslationResult();
+
+            _queryTranslator.Translate(expression, result);
+
+            IEnumerable<EntityAdapter<TEntity>> tableEntities = _repository.ExecuteQuery<EntityAdapter<TEntity>>(result.TableQuery);
+
+            return GetProcessedResult(tableEntities, result);
+        }
+
+        /// <summary>
+        ///     Executes expression query asynchronously.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<object> ExecuteAsync(
+            Expression expression,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            var result = new TranslationResult();
+
+            _queryTranslator.Translate(expression, result);
+
+            return _repository
+                .ExecuteQueryAsync<EntityAdapter<TEntity>>(result.TableQuery, cancellationToken)
+                .Then(p => GetProcessedResult(p, result), cancellationToken);
+        }
+
+        /// <summary>
+        ///     Executes post processing of retrieved entities.
+        /// </summary>
+        /// <param name="tableEntities">Table entities.</param>
+        /// <param name="translation">translation result.</param>
+        /// <returns>Collection of entities.</returns>
+        private object GetProcessedResult(IEnumerable<EntityAdapter<TEntity>> tableEntities, TranslationResult translation)
+        {
+            IEnumerable<TEntity> result = tableEntities.Select(q => KeepState(q));
+
+            if (translation.PostProcessing == null)
+            {
+                return result;
+            }
+
+            try
+            {
+                return translation.PostProcessing.DynamicInvoke(result.AsQueryable());
+            }
+            catch (TargetInvocationException e)
+            {
+                throw e.InnerException;
+            }
+        }
+
+        private TEntity KeepState(EntityAdapter<TEntity> entity)
+        {
+            //foreach (var entity in enumerable)
+          //  {
+                //TODO: key should properly be row+part key
+                _entityConfiguration.EntityStates.AddOrUpdate(entity.InnerObject.GetHashCode(),
+                    (key) => new Tuple<DateTimeOffset, string>(entity.Timestamp, entity.ETag), (key,v) => new Tuple<DateTimeOffset, string>(entity.Timestamp, entity.ETag));
+                _entityConfiguration.ReverseKeyMapping<TEntity>(entity);
+        //    }
+        //        return enumerable;
+                return entity.InnerObject;
+        }
+    }
+}
